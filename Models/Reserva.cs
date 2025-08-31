082ad9ff5a3ae4875216383d5312791b0ea32d6a
RCBento <andrexedu@gmail.com>
2025-08-31 14:48:39 -0300

    Implementações Realizadas na Classe - Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido - OK
+            // *FEITO*
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido - OK
+                // *FEITO*
+                throw new ArgumentException("A quantidade de hóspedes excede a capacidade da suíte.");
             }
         }
 
@@ -37,21 +38,21 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // TODO: Retorna o valor da diária - OK
+            // Cálculo: DiasReservados X Suite.ValorDiaria - OK
+            // *FEITO*
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10% - OK
+            // *FEITO*
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9M;
             }
 
             return valor;
